7b4a067f50c9913d888deab4d82280cf76c6fe07
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2024-01-30 00:57:33 +0530

    push

--- Assignment Repo/React Day-V Assignment/Network-Requast/src/App.jsx
+++ Assignment Repo/React Day-V Assignment/Network-Requast/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [post,setpost] = useState([]);
+  const [loading,isloading] = useState(false);
+  const [error,Iserror] = useState()
+  const getdata = async()=>{
+    try {
+      isloading('Data is Being Loading')
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+          const fineldata = await res.json()
+      setpost(fineldata)
+      isloading("Data is Loaded")
+    } 
+    catch (error) {
+      Iserror('something went Wrong')
+    }
+   
+useEffect(()=>{
+  getdata()
+},[])
+console.log(post);
+  }
+ return(
+  <>
+  <h1>Fetch User Data With Use Effect</h1>
+  </>
+ )
+   
 }
 
 export default App
